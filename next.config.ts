14c77086e41a52d7966802cfb5233ea9c247df03
Avadhi Verma <avadhiverma2002@gmail.com>
2025-07-01 22:19:22 +0530

    Prepared repo to push changes to main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ output: 'export',
+  images: {
+    unoptimized: true, 
+  },
+  basePath: '/Flipr_Project',  
+  assetPrefix: '/Flipr_Project',
 };
 
 export default nextConfig;
